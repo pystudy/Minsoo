ffd7c6ec2dd3c2fb05da9a4a42ea85a12fbd0a45
NakOh <kk0704@naver.com>
2015-01-07 11:31:35 +0900

    Repository정리중

--- Spring/src/springbook/user/dao/DaoFactory.java
+++
@@ -1,9 +0,0 @@
-package springbook.user.dao;
-
-public class DaoFactory {
-	public UserDao userDao(){
-		ConnectionMaker connectionMaker = new DconnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
-	}
-}
